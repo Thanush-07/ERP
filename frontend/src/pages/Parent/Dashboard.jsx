f153a6c40146201b0f47fe678129ba2d302dc85e
Thanush-07 <thanushkumar722@gmail.com>
2025-12-11 17:22:47 +0530

    company modules updated

---
+++ frontend/src/pages/Parent/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Parent Dashboard</h1>
+      {/* Additional dashboard content goes here */}
+    </div>
+  );
+}
+export default Dashboard;
