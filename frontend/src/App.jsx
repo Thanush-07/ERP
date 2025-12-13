a75851cc98c6c3c7c9def63b5864c12d63efa722
Thanush-07 <thanushkumar722@gmail.com>
2025-12-13 19:05:16 +0530

    institution routes updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,17 @@ import CompanyAdminDashboard from "./pages/Company_admin/Dashboard";
 import Institutions from "./pages/Company_admin/Institutions";
 import Users from "./pages/Company_admin/Users";
 import GlobalReport from "./pages/Company_admin/GlobalReport";
+
+// Institution admin
+import InstitutionLayout from "./pages/Institution_admin/InstitutionLayout";
 import InstitutionDashboard from "./pages/Institution_admin/Dashboard";
+import InstitutionBranches from "./pages/Institution_admin/Branches";
+// TODO: create these files/components:
+import BranchAdmins from "./pages/Institution_admin/Branches";
+import InstitutionReports from "./pages/Institution_admin/Reports";
+import ChangePassword from "./pages/Institution_admin/ChangePassword";
+
+// Other roles
 import BranchAdminDashboard from "./pages/Branch_admin/Dashboard";
 import StaffDashboard from "./pages/Staff/Dashboard";
 import ParentDashboard from "./pages/Parent/Dashboard";
@@ -26,19 +36,29 @@ function App() {
         {/* Company admin module with layout */}
         <Route path="/company-admin" element={<CompanyLayout />}>
           <Route path="dashboard" element={<CompanyAdminDashboard />} />
-          <Route path="Institutions" element={<Institutions />} />
+          <Route path="institutions" element={<Institutions />} />
           <Route path="users" element={<Users />} />
           <Route path="report" element={<GlobalReport />} />
         </Route>
 
         {/* Institution admin module with layout */}
-        <Route path="/InstitutionDashboard" element={<InstitutionDashboard />} />  
-        <Route path="/institution/*" element={<InstitutionLayout />} />
- {/* Other roles */}
-        <Route path="/branch-admin/dashboard" element={<BranchAdminDashboard />} />
+        <Route path="/institution" element={<InstitutionLayout />}>
+          <Route path="dashboard" element={<InstitutionDashboard />} />
+          <Route path="branches" element={<InstitutionBranches />} />
+          <Route path="branch-admins" element={<BranchAdmins />} />
+          <Route path="reports" element={<InstitutionReports />} />
+          <Route path="change-password" element={<ChangePassword />} />
+        </Route>
+
+        {/* Other roles */}
+        <Route
+          path="/branch-admin/dashboard"
+          element={<BranchAdminDashboard />}
+        />
         <Route path="/staff/dashboard" element={<StaffDashboard />} />
         <Route path="/parent/dashboard" element={<ParentDashboard />} />
 
+        {/* Fallback */}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
